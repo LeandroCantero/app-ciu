6a2de0d578a3af7f895632cc5b3fe27fed007e94
LeandroCantero <lcantero18@gmail.com>
2023-06-01 17:35:11 -0300

    test

---
+++ src/components/Body/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam unde totam, porro est maxime nemo, praesentium expedita labore magnam vel sit amet harum rem, ad quidem eveniet. Libero, ipsam reprehenderit?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam unde totam, porro est maxime nemo, praesentium expedita labore magnam vel sit amet harum rem, ad quidem eveniet. Libero, ipsam reprehenderit?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam unde totam, porro est maxime nemo, praesentium expedita labore magnam vel sit amet harum rem, ad quidem eveniet. Libero, ipsam reprehenderit?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam unde totam, porro est maxime nemo, praesentium expedita labore magnam vel sit amet harum rem, ad quidem eveniet. Libero, ipsam reprehenderit?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam unde totam, porro est maxime nemo, praesentium expedita labore magnam vel sit amet harum rem, ad quidem eveniet. Libero, ipsam reprehenderit?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam unde totam, porro est maxime nemo, praesentium expedita labore magnam vel sit amet harum rem, ad quidem eveniet. Libero, ipsam reprehenderit?
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Components.js
@@ -0,0 +1,15 @@
+import Navbar from "./Header/Navbar"
+import Main from "./Body/Main"
+import Footer from "./Footer/Footer"
+
+const Components = () => {
+    return (
+      <div>
+        <Navbar />
+        <Main/>
+        <Footer/>
+      </div>
+    )
+  }
+  
+  export default Components
